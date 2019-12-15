abe3938754a47712331b07ccec98beed05a83d4e
Sergio Sánchez Sánchez <ssanchez@ebikemotion.com>
2019-12-15 15:00:51 +0100

    AGR-020: Implement user profile editing screen.

--- app/src/main/java/sanchez/sanchez/sergio/agrociety/ui/features/main/settings/UserSettingsFragment.kt
+++ app/src/main/java/sanchez/sanchez/sergio/agrociety/ui/features/main/settings/UserSettingsFragment.kt
@@ -1,6 +1,8 @@
 package sanchez.sanchez.sergio.agrociety.ui.features.main.settings
 
+import android.Manifest
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.google.android.material.picker.MaterialDatePicker
 import kotlinx.android.synthetic.main.fragment_user_settings.*
@@ -8,12 +10,10 @@ import sanchez.sanchez.sergio.agrociety.R
 import sanchez.sanchez.sergio.agrociety.di.components.fragment.UserSettingsComponent
 import sanchez.sanchez.sergio.agrociety.di.factory.DaggerComponentFactory
 import sanchez.sanchez.sergio.agrociety.ui.features.intro.IntroActivity
-import sanchez.sanchez.sergio.brownie.extension.navigateAndFinish
-import sanchez.sanchez.sergio.brownie.extension.popBackStack
+import sanchez.sanchez.sergio.brownie.extension.*
 import sanchez.sanchez.sergio.brownie.ui.core.activity.SupportActivity
 import sanchez.sanchez.sergio.brownie.ui.core.fragment.SupportFragment
-
-
+import timber.log.Timber
 
 class UserSettingsFragment: SupportFragment<UserSettingsViewModel, Void>(UserSettingsViewModel::class.java) {
 
@@ -31,9 +31,20 @@ class UserSettingsFragment: SupportFragment<UserSettingsViewModel, Void>(UserSet
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        toolbar.setNavigationOnClickListener {
-            popBackStack()
+        toolbar.apply {
+            setNavigationOnClickListener {
+                popBackStack()
+            }
+            setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.saveChanges ->
+                        onSaveChanges()
+                }
+                true
+            }
         }
+
+
 
         closeSession.setOnClickListener {
             navigateAndFinish(IntroActivity.createDestination(requireActivity()))
@@ -41,12 +52,65 @@ class UserSettingsFragment: SupportFragment<UserSettingsViewModel, Void>(UserSet
 
         userBirthday.setOnClickListener {
 
-            val builder = MaterialDatePicker.Builder.datePicker()
-            builder.setTitleTextResId(R.string.user_settings_birthday_picker_title)
-            val picker = builder.build()
+            val picker = MaterialDatePicker.Builder.datePicker().apply {
+                setTitleTextResId(R.string.user_settings_birthday_picker_title)
+            }.build()
+
+            picker.addOnDismissListener {
+                picker.selection?.let { currentSelection ->
+                    userBirthday.setText(currentSelection.toDateStringWithAge(requireContext()))
+                }
+            }
             picker.show(childFragmentManager, picker.toString())
 
         }
 
+        changePrimaryPhoto.setOnClickListener {
+            onChangePhoto()
+        }
+
+        changeSecondaryPhoto.setOnClickListener {
+            onChangePhoto()
+        }
     }
+
+
+    override fun onSinglePermissionGranted(permission: String) {
+        super.onSinglePermissionGranted(permission)
+        Timber.d("onSinglePermissionGranted -> $permission")
+    }
+
+    override fun onSinglePermissionRejected(permission: String) {
+        super.onSinglePermissionRejected(permission)
+        Timber.d("onSinglePermissionRejected -> $permission")
+    }
+
+    override fun onErrorOccurred(permission: String) {
+        super.onErrorOccurred(permission)
+        Timber.d("onErrorOccurred -> $permission")
+    }
+
+    /**
+     * Private Methods
+     */
+
+    private fun onSaveChanges() {
+        showProgressDialog(R.string.progress_please_wait)
+        Handler().postDelayed({
+            hideProgressDialog()
+            showNoticeDialog(R.string.user_settings_profile_saved_successful, true)
+        }, 2000)
+    }
+
+    /**
+     * On Change Photo
+     */
+    private fun onChangePhoto() {
+        if(permissionManager.shouldAskPermission(Manifest.permission.CAMERA)) {
+            permissionManager.checkSinglePermission(
+                permission = Manifest.permission.CAMERA,
+                reasonText = "")
+        }
+    }
+
 }
