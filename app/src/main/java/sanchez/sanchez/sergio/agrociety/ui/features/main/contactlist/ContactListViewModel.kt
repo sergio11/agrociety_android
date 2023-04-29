8991d929ea1e8410959be461f5fe3c58b99fa0f3
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-04-29 15:37:59 +0200

    Update ContactListViewModel.kt

--- app/src/main/java/sanchez/sanchez/sergio/agrociety/ui/features/main/contactlist/ContactListViewModel.kt
+++ app/src/main/java/sanchez/sanchez/sergio/agrociety/ui/features/main/contactlist/ContactListViewModel.kt
@@ -10,8 +10,8 @@ class ContactListViewModel @Inject constructor(): SupportGroupedLCEViewModel<Use
     private val contactList = mutableListOf(
         User(
             id = "dsadsadsadsadas",
-            displayName = "David Martín Fidalgo",
-            email = "davidmartin@gmail.com",
+            displayName = "User 1",
+            email = "user1@gmail.com",
             followers = 32,
             follow = 23,
             photoUrl = "https://media.licdn.com/dms/image/C5603AQFHfMzxZg-B1Q/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=s1HgcSfcUNKYizZwFyrCyp30YzJuFsErYrOd_uY9XXM",
@@ -19,53 +19,53 @@ class ContactListViewModel @Inject constructor(): SupportGroupedLCEViewModel<Use
         ),
         User(
             id = "adsar3werfdsfseewqe",
-            displayName = "Francisco Terrones",
+            displayName = "User 2",
             followers = 12,
             follow = 65,
-            email = "franciscoterrones@gmail.com",
+            email = "user2@gmail.com",
             photoUrl = "https://media.licdn.com/dms/image/C4E03AQEYDOEIrBJA4g/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=ok6zwYaPdL24TzU0NNZNIeJnTqLRi5WGL4MXl-lgb8E",
             background = R.drawable.publication_example_two
         ),
         User(
             id = "dsadsarewrewrewvcxvs",
-            displayName = "Francisco Roldán Córdoba",
+            displayName = "User 3",
             followers = 23,
             follow = 54,
-            email = "franciscoroldan@gmail.com",
+            email = "user3@gmail.com",
             photoUrl = "https://media.licdn.com/dms/image/C4D03AQF7f1_fsB8L9Q/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=IXIrlCuYFwLLh86KW22VCQX1_EGQty-hUoPWxnWTCmQ",
             background = R.drawable.publication_example_tree
         ),
         User(
             id = "dafdsf789478392hklfhsd",
-            displayName = "Sergio Gonzalez",
+            displayName = "User 4",
             followers = 12,
             follow = 5,
-            email = "sergiogonzalez@gmail.com",
+            email = "user4@gmail.com",
             photoUrl = "https://media.licdn.com/dms/image/C5603AQH-UJpm-rC6Vw/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=by5w9yrq3fkoRwPFBMyStEkFz4WANDHixto4KX2nbwA",
             background = R.drawable.publication_example_four
         ),
         User(
             id = "dasdsadsa432423bfdgfdg",
-            displayName = "Jorge Martínez Marí",
-            email = "jorgemartinezmari@gmail.com",
+            displayName = "User 5",
+            email = "user5@gmail.com",
             photoUrl = "https://media.licdn.com/dms/image/C4D03AQHwTcBjN6oGuQ/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=iMRyoVwBszKQ0TJQS8EicvoEVVXRhFlyXZNiU5FbafI",
             background = R.drawable.publication_example_one
         ),
         User(
             id = "dsadas3423432ghgfhgf",
-            displayName = "Gema Conde Gonzalez",
+            displayName = "User 6",
             followers = 32,
             follow = 23,
-            email = "gemacondegonzalez@gmail.com",
+            email = "user6@gmail.com",
             photoUrl = "https://media.licdn.com/dms/image/C4E03AQHfhXsFSZsxdA/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=xYXQ7M7oDmY_jX4UtsMOW_gt7GgKQ-MbkpHM_Te1XfM",
             background = R.drawable.publication_example_two
         ),
         User(
             id = "4324324sfgsgfdgfd",
-            displayName = "Mª Gema Jiménez Fernández",
+            displayName = "User 7",
             followers = 22,
             follow = 22,
-            email = "gemajimenezfernandez@gmail.com",
+            email = "user7@gmail.com",
             photoUrl = "https://media.licdn.com/dms/image/C5603AQETtcBGB63hAw/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=BMuG3ElNVFxeuHro5U9-JjngI5XwSvlMp78EjAZyvFA",
             background = R.drawable.publication_example_tree
         )
